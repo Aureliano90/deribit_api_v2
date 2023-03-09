a8a3ccc9e0e443b9015fef06cca268d898ae8c1c
Aureliano <81753529+aureliano90@users.noreply.github.com>
2023-03-08 16:29:24 -0800

    Change `TypedDict` declaration

--- dtypes.py
+++ dtypes.py
@@ -1,32 +1,32 @@
 from typing import Any, Dict, List, Optional
 from web3.types import Literal, RPCResponse, TypedDict
 
-Authentication = TypedDict('Authentication', {
-    'access_token': str,
-    'expires_in': int,
-    'refresh_token': str,
-    'scope': str,
-    'token_type': Literal['bearer']
-})
 
-Currency = TypedDict('Currency', {
-    'coin_type': str,
-    'currency': str,
-    'currency_long': str,
-    'fee_precision': int,
-    'min_confirmations': int,
-    'min_withdrawal_fee': float,
-    'withdrawal_fee': float,
-    'withdrawal_priorities': List
-})
+class Authentication(TypedDict):
+    access_token: str
+    expires_in: int
+    refresh_token: str
+    scope: str
+    token_type: Literal['bearer']
 
-ChartData = TypedDict('ChartData', {
-    'close': List[float],
-    'cost': List[float],
-    'high': List[float],
-    'low': List[float],
-    'open': List[float],
-    'status': Literal['ok', 'no_data'],
-    'ticks': List[int],
-    'volume': List[float]
-})
+
+class Currency(TypedDict):
+    coin_type: str
+    currency: str
+    currency_long: str
+    fee_precision: int
+    min_confirmations: int
+    min_withdrawal_fee: float
+    withdrawal_fee: float
+    withdrawal_priorities: List
+
+
+class ChartData(TypedDict):
+    close: List[float]
+    cost: List[float]
+    high: List[float]
+    low: List[float]
+    open: List[float]
+    status: Literal['ok', 'no_data']
+    ticks: List[int]
+    volume: List[float]
